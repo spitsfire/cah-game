7dba53da6e548ceca17bfe78e67f95cb84465857
Mary Claire Elliott <yue.katou@gmail.com>
2020-05-14 15:19:46 -0500

    set up Header and Lobby component in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Lobby from "./components/pages/Lobby";
+import Header from "./components/layout/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Lobby />
+    </div>
+  );
 }
 
 export default App;
